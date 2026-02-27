021183a9eaeb5e514b86c3a7f4e40e89ff43fefa
AnetReyes25 <anetreyes19@gmail.com>
2026-02-27 20:02:11 +0000

    lab 15

--- File/sys-admin.py
+++ File/sys-admin.py
@@ -7,3 +7,12 @@ subprocess.run(["ls", "-l"])
 
 
 subprocess.run(["ls","-l","README.md"])
+
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
